9bf5d4019a574c5f5e0b6d27ac2f997bf887fc59
Fran Vazquez Garcia <fjvazquezg011@gmail.com>
2018-10-17 11:33:26 +0200

    Controlador de la eleccion finalizado

---
+++ src/main/java/es/albarregas/beans/EleccionBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author fjvaz
+ */
+public class EleccionBean implements Serializable{
+    private boolean edificio;
+    private boolean contenido;
+
+    public boolean isEdificio() {
+        return edificio;
+    }
+
+    public void setEdificio(boolean edificio) {
+        this.edificio = edificio;
+    }
+
+    public boolean isContenido() {
+        return contenido;
+    }
+
+    public void setContenido(boolean contenido) {
+        this.contenido = contenido;
+    }
+    
+    
+}
